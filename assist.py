974a5248777c7e1e2c245061596bd41091eeb3cf
LocalNeckbeard <118300270+LocalNeckbeard@users.noreply.github.com>
2024-03-25 15:55:57 -0400

    Updated Formating and Comments

--- assist.py
+++ assist.py
@@ -12,33 +12,30 @@ def intAssist(client): #Creates the assistant thread. could be improved by inste
     name="Pai", #Just the name of the AI, has no effect
     instructions="You are to answer the questions asked to the best of your ability. Answer as if you are speaking directly to a person. Keep your answers under three paragraphs. You act friendly to whoever asks a question.", 
     #Here we can adjust the personality of the AI. This can allow for any personality. Unsure if this involves GPT filtering. 
-    #Could also be interesting to allow for a prewritten file to be imported for the instructions. 
     tools=[{"type": "code_interpreter"}], #If we want to add math/code functionality, will slow down response
     model="gpt-4-1106-preview" #to change the model if we desire
-) #In the future, it might be best to instead import an already existing assistant. As of now, it creates a new one every time. 
+)   
     thread = client.beta.threads.create()
     return thread,assistant
-
+    #Could also be interesting to allow for a prewritten file to be imported for the instructions. 
+    #In the future, it might be best to instead import an already existing assistant. As of now, it creates a new one every time.
 def addmsg(msg,thread,assistant,client,x):
-    message = client.beta.threads.messages.create( #sends a message to the assistant thread
+    message = client.beta.threads.messages.create( #sends the message to the assistant thread
     thread_id=thread.id,
     role="user",
     content=msg
 )
     run = client.beta.threads.runs.create(thread_id=thread.id,assistant_id=assistant.id,) #runs the assistant to process our input
-    while run.status == "queued" or run.status == "in_progress": #Waits until GPT is done processing, and is ready to output
-        run = client.beta.threads.runs.retrieve(
-            thread_id=thread.id,
-            run_id=run.id,
-        )
-        time.sleep(0.5)
-    messages = client.beta.threads.messages.list(thread_id=thread.id, order="asc") #grab the message
+    while run.status == "queued" or run.status == "in_progress": #Run this loop if the status isn't completed
+        run = client.beta.threads.runs.retrieve(thread_id=thread.id,run_id=run.id,)#Grabs the current status 
+        time.sleep(0.5) #Waits
+    messages = client.beta.threads.messages.list(thread_id=thread.id, order="asc") #grabs the message
 #messages in assistants are stored in these 2d arrays
 #under data, even numbers are our input, and odd numbers are the output. This is why x starts at 1, and increases by 2
     print(messages.data[x].content[0].text.value) 
     speech_file_path = Path(__file__).parent / "speech.mp3"  #Creates a voice file to output
     response = client.audio.speech.create(model="tts-1",voice="alloy",input=messages.data[x].content[0].text.value) #Converts the Output into an Audio File
-    response.stream_to_file(speech_file_path) #uses GPT to generate it from the output
+    response.stream_to_file(speech_file_path) #uses OpenAI Voice to generate it from the output
     return 
 
 
